f183a6afa857724e361e00d8cdceeff7321c8793
LeixB <abone9999@gmail.com>
2023-12-07 13:06:13 +0100

    Day6 parsing

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,19 +1,43 @@
 module Day6 where
 
-import Relude
+import Data.Char (isDigit)
+import Relude hiding (get)
+import Relude.Unsafe (read)
+import Text.ParserCombinators.ReadP
+import Prelude (readFile)
 
-polyroots :: Double -> Double -> (Int, Int)
-polyroots b c =
-  let a = -1.0
+polyroots :: (Integral a) => a -> a -> (Int, Int)
+polyroots b' c' =
+  let a = -1.0 :: Double
+      b = fromIntegral b'
+      c = fromIntegral c'
       sd = sqrt $ b * b - 4 * a * (-c)
-   in (floor $ (-b - sd) / (2 * a), ceiling $ (-b + sd) / (2 * a))
+   in (ceiling $ (-b - sd) / (2 * a), floor $ (-b + sd) / (2 * a))
 
-input = ([48, 93, 85, 95], [296, 1928, 1236, 1391])
+solve :: [(Int, Int)] -> Int
+solve = product . fmap ((pred . uncurry (-)) . uncurry polyroots)
 
-part1 = uncurry solve input
-  where
-    solve xs ys = product $ fmap pred $ zipWith polyroots xs ys <&> uncurry (-)
+part1, part2 :: String -> Maybe Int
+part1 = fmap solve . runParser parseInput
+part2 = part1 . filter (/= ' ')
 
-input2 = (48938595, 296192812361391)
+-- Parsing
 
-part2 = uncurry (-) . uncurry polyroots $ input2
+runParser :: ReadP a -> String -> Maybe a
+runParser p s = case readP_to_S p s of
+  [(a, "")] -> Just a
+  _ -> Nothing
+
+parseInput :: (Read a) => ReadP [(a, a)]
+parseInput = zip <$> parseLine <*> parseLine
+
+parseLine :: (Read a) => ReadP [a]
+parseLine = skipMany1 (satisfy (/= ':')) *> char ':' *> skipSpaces *> many1 (satisfy isDigit) `sepBy` many1 (char ' ') <* char '\n' <&> fmap read
+
+-- IO
+
+run1 :: FilePath -> IO (Maybe Int)
+run1 file = part1 <$> Prelude.readFile file
+
+run2 :: FilePath -> IO (Maybe Int)
+run2 file = part1 <$> Prelude.readFile file
