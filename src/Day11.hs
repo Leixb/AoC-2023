a9b110fc3fdbefe2044f7cd8e42676ae05a0837b
LeixB <abone9999@gmail.com>
2023-12-11 13:33:28 +0100

    Day11 part2

--- src/Day11.hs
+++ src/Day11.hs
@@ -28,9 +28,16 @@ distance :: [Int] -> [Int] -> (Int, Int) -> (Int, Int) -> Int
 -- distance emptyC emptyR (h1, w1) (h2, w2) | w1 > w2 = distance emptyC emptyR (h1, w2) (h2, w1)
 distance emptyC emptyR (h1, w1) (h2, w2) = distCoord emptyC h1 h2 + distCoord emptyR w1 w2
 
+distance' :: Int -> [Int] -> [Int] -> (Int, Int) -> (Int, Int) -> Int
+distance' n emptyC emptyR (h1, w1) (h2, w2) = distCoord' n emptyC h1 h2 + distCoord' n emptyR w1 w2
+
 distCoord :: [Int] -> Int -> Int -> Int
 distCoord emptyC c1 c2 | c1 > c2 = distCoord emptyC c2 c1
 distCoord emptyC c1 c2 = c2 - c1 + length (filter (\c -> c1 < c && c < c2) emptyC)
+
+distCoord' :: Int -> [Int] -> Int -> Int -> Int
+distCoord' n emptyC c1 c2 | c1 > c2 = distCoord' n emptyC c2 c1
+distCoord' n emptyC c1 c2 = c2 - c1 + (n - 1) * length (filter (\c -> c1 < c && c < c2) emptyC)
 
 allPairs :: [a] -> [(a, a)]
 allPairs [] = []
@@ -54,3 +61,12 @@ printGrid (Just g) = do
       putStr $ if g ! (h, w) then "#" else "."
     putStrLn ""
 printGrid Nothing = putStrLn "Invalid grid"
+
+part2 n g =
+  let galaxies = allLocations g
+      emptyC = findEmptyCols g
+      emptyR = findEmptyRows g
+
+      pairs = allPairs galaxies
+      res = map (uncurry $ distance' n emptyR emptyC) pairs
+   in res
