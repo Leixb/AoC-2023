8d773a706462d5f1778bf200c5491aa0901bed82
LeixB <abone9999@gmail.com>
2023-12-12 17:42:39 +0100

    Day12 recursive

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,7 +1,7 @@
 module Day12 where
 
+import Data.Array
 import Data.Char (isDigit)
-import Data.List (dropWhileEnd)
 import Relude hiding (get, many)
 import Relude.Unsafe (read)
 import Text.ParserCombinators.ReadP
@@ -44,18 +44,58 @@ buildParser l = do
 combinations :: Spring -> Int
 combinations (s, l) = length $ readP_to_S (buildParser l) s
 
-part1 :: Problem -> Int
-part1 = sum . fmap combinations
+-- part1 :: Problem -> Int
+-- part1 = sum . fmap combinations
+part1 = fmap combinations
 
-run1 :: FilePath -> IO Int
+-- run1 :: FilePath -> IO Int
 run1 f = readFileBS f >>= maybe (fail "parse error") (return . part1) . parse
 
 -- Brute force approach
 part2' :: Problem -> Int
 part2' = sum . fmap (combinations . bimap (join . intersperse "?" . replicate 5) (join . replicate 5))
 
-part2 :: Problem -> Int
-part2 = undefined
-
-run2 :: FilePath -> IO Int
+-- run2 :: FilePath -> IO Int
 run2 f = readFileBS f >>= maybe (fail "parse error") (return . part2) . parse
+
+type Spring' = ([Status], [Int])
+
+type Problem' = [Spring']
+
+toSpring' :: Spring -> Spring'
+toSpring' (s, l) = (fmap toStatus s, l)
+  where
+    toStatus :: Char -> Status
+    toStatus '.' = Good
+    toStatus '#' = Bad
+    toStatus '?' = Unknown
+    toStatus _ = error "impossible"
+
+-- recursive approach
+-- part2 :: Problem -> Int
+-- part2 = sum . fmap (combinations' . toSpring')
+part2 = fmap (combinations' . toSpring')
+
+isGood :: Status -> Bool
+isGood Bad = False
+isGood _ = True
+
+isBad :: Status -> Bool
+isBad Good = False
+isBad _ = True
+
+combinations' :: Spring' -> Int
+combinations' (s, l) = go s l
+  where
+    go :: [Status] -> [Int] -> Int
+    go s' [] = bool 0 1 (all isGood s')
+    go (Unknown : ss) x = go (Good : ss) x + go (Bad : ss) x
+    go (Good : ss) x = go ss x
+    go (Bad : ss) (x : xs)
+      | all isBad bads && length bads == x - 1 && maybe True isGood g = go rest' xs
+      | otherwise = 0
+      where
+        (bads, rest) = splitAt (x - 1) ss
+        g = viaNonEmpty head rest
+        rest' = fromMaybe [] (viaNonEmpty tail rest)
+    go _ _ = 0
