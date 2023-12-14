4014e2235b07f3d30a653d668264f1799d5c5f2b
LeixB <abone9999@gmail.com>
2023-12-14 11:50:11 +0100

    Day14 part 2 naive

--- src/Day14.hs
+++ src/Day14.hs
@@ -8,8 +8,9 @@ type Problem = [ByteString]
 parse :: ByteString -> Problem
 parse = BS.split '\n'
 
+-- Our "north" is to the right, since it makes the problem easier
 faceNorth :: Problem -> Problem
-faceNorth = rotate . rotate . BS.transpose
+faceNorth = rotate
 
 move :: Problem -> Problem
 move = fmap (BS.intercalate "#" . fmap BS.sort . BS.split '#')
@@ -21,4 +22,22 @@ part1 :: ByteString -> Int
 part1 = sum . join . count . move . faceNorth . parse
 
 rotate :: Problem -> Problem
-rotate = BS.transpose . fmap BS.reverse
+rotate = fmap BS.reverse . BS.transpose
+
+-- Anti-clockwise
+rotate' :: Problem -> Problem
+rotate' = BS.transpose . fmap BS.reverse
+
+display :: Problem -> IO ()
+display = traverse_ (putStrLn . BS.unpack)
+
+rotMov :: Problem -> Problem
+rotMov = rotate . move
+
+doCycle :: Problem -> Problem
+doCycle = rotMov . rotMov . rotMov . rotMov
+
+doNcycles :: Int -> Problem -> Problem
+doNcycles n = foldr (.) id (replicate n doCycle)
+
+part2 = fmap count . iterate doCycle . faceNorth . parse
