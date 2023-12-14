4d20c917a5f2bc5d2c4b3fb2c0d3fba6907dca15
LeixB <abone9999@gmail.com>
2023-12-14 13:13:23 +0100

    Fetch problem inputs

--- src/AoC.hs
+++ src/AoC.hs
@@ -1,31 +1,40 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module AoC
-  ( module Day1,
-    runSolution,
+  ( getInput,
+    getInputBS,
+    getInputString,
     Problem (..),
     year,
     day,
-    part,
   )
 where
 
 import Control.Lens
-import Data.Text
-import Day1
-import Network.Wreq
+import qualified Data.ByteString as BS
+import qualified Data.Text as T
+import Network.Wreq (defaults, getWith, header, responseBody)
 import Relude hiding (get)
 
 data Problem = Problem
   { _year :: Int,
-    _day :: Int,
-    _part :: Int
+    _day :: Int
   }
 
 makeLenses ''Problem
 
-runSolution :: Problem -> (Text -> a) -> IO a
-runSolution p f = do
+getCookie :: IO ByteString
+getCookie = readFileBS "cookie.txt" <&> ("session=" <>) . BS.init
+
+getInputBS :: Problem -> IO ByteString
+getInputBS p = do
   let url = "https://adventofcode.com/" ++ show (p ^. year) ++ "/day/" ++ show (p ^. day) ++ "/input"
-  r <- get url
-  return . f . decodeUtf8 $ r ^. responseBody
+  cookie <- getCookie
+  r <- getWith (defaults & header "Cookie" .~ [cookie]) url
+  return . toStrict $ r ^. responseBody
+
+getInput :: Problem -> IO Text
+getInput = fmap decodeUtf8 . getInputBS
+
+getInputString :: Problem -> IO String
+getInputString = fmap T.unpack . getInput
