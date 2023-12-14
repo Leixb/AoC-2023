abb61b7f16517a954c929a4aa3637a3e47d8779b
LeixB <abone9999@gmail.com>
2023-12-14 11:25:26 +0100

    Day14 split part1 into different functions

--- src/Day14.hs
+++ src/Day14.hs
@@ -3,5 +3,22 @@ module Day14 where
 import qualified Data.ByteString.Char8 as BS
 import Relude
 
+type Problem = [ByteString]
+
+parse :: ByteString -> Problem
+parse = BS.split '\n'
+
+faceNorth :: Problem -> Problem
+faceNorth = rotate . rotate . BS.transpose
+
+move :: Problem -> Problem
+move = fmap (BS.intercalate "#" . fmap BS.sort . BS.split '#')
+
+count :: Problem -> [[Int]]
+count = fmap (fmap succ . BS.elemIndices 'O')
+
 part1 :: ByteString -> Int
-part1 = sum . fmap (sum . fmap succ . BS.elemIndices 'O' . BS.reverse . BS.intercalate "#" . fmap (BS.reverse . BS.sort) . BS.split '#') . BS.transpose . BS.split '\n'
+part1 = sum . join . count . move . faceNorth . parse
+
+rotate :: Problem -> Problem
+rotate = BS.transpose . fmap BS.reverse
