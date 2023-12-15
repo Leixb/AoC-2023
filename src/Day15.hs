3529de5e3ebfbe76e8ba151c2a875042facc180f
LeixB <abone9999@gmail.com>
2023-12-15 15:23:20 +0100

    Day 15 golfed

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TupleSections #-}
-
 module Day15 where
 
 import Data.Array
@@ -9,17 +7,17 @@ import Relude
 import qualified Relude.Unsafe as Unsafe
 import Text.ParserCombinators.ReadP hiding (get)
 
-part1 :: BS.ByteString -> Int
-part1 = sum . fmap (hash . BS.unpack) . BS.split ',' . BS.dropEnd 1
-
 hash :: String -> Int
-hash = foldl' step 0 . fmap ord
-  where
-    step prev n = (prev + n) * 17 `mod` 256
+hash = foldl' (\a x -> (a + x) * 17 `mod` 256) 0 . fmap ord
+
+part1 :: ByteString -> Int
+part1 = sum . fmap (hash . BS.unpack) . BS.split ',' . BS.dropEnd 1
 
 -- Part 2
 
 type Problem = [Operation]
+
+type S = Array Int [(String, Int)]
 
 data Operation = Set String Int | Remove String deriving (Show)
 
@@ -27,42 +25,20 @@ parse :: BS.ByteString -> Maybe Problem
 parse = fmap fst . viaNonEmpty last . readP_to_S parse' . BS.unpack
   where
     parse' = sepBy parseOperation (char ',') <* char '\n' <* eof
+    parseOperation =
+      munch1 isAlpha
+        >>= \label -> (Remove label <$ char '-') +++ (Set label . Unsafe.read <$> (char '=' *> munch1 isDigit))
 
-    parseOperation = do
-      label <- munch1 isAlpha
-      (Remove label <$ char '-') +++ (Set label . Unsafe.read <$> (char '=' *> munch1 isDigit))
+liftOp :: Operation -> Endo S
+liftOp (Set label v) = Endo $ \s ->
+  let (b, a) = second (drop 1) $ span ((/= label) . fst) (s ! hash label)
+   in s // [(hash label, b <> [(label, v)] <> a)]
+liftOp (Remove l) = Endo $ \s -> s // [(hash l, filter ((/= l) . fst) (s ! hash l))]
 
-type App = State (Array Int [(String, Int)])
+score :: S -> Int
+score m = sum $ join [(* (i + 1)) <$> zipWith (*) [1 ..] (snd <$> (m ! i)) | i <- [0 .. 255]]
 
-doOperation :: Operation -> App ()
-doOperation op = do
-  m <- gets (! idx)
-  let (b, xa) = span ((/= label) . fst) m
-      a = fromMaybe [] $ viaNonEmpty tail xa
-  modify
-    $ flip
-      (//)
-      [ ( idx,
-          case op of
-            Set _ value -> b <> [(label, value)] <> a
-            Remove _ -> b <> a
-        )
-      ]
-  where
-    getLabelAndIdx (Set l _) = (l, hash l)
-    getLabelAndIdx (Remove l) = (l, hash l)
-    (label, idx) = getLabelAndIdx op
-
-score :: App [Int]
-score = do
-  m <- get
-  pure $ join [(* (i + 1)) <$> multByIdx (snd <$> (m ! i)) | i <- [0 .. 255]]
-  where
-    multByIdx = zipWith (*) [1 ..]
-
-part2 :: BS.ByteString -> Maybe Int
+part2 :: ByteString -> Maybe Int
 part2 input = do
-  ops <- parse input
-  pure . sum $ evalState (traverse_ doOperation ops *> score) initialState
-  where
-    initialState = listArray (0, 256) $ repeat []
+  ops <- appEndo . foldMap liftOp . reverse <$> parse input
+  pure . score . ops . listArray (0, 255) $ repeat []
