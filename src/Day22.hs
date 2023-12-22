c8cd077769a9f621f528e754c37ed87a1784180b
LeixB <abone9999@gmail.com>
2023-12-22 19:15:22 +0100

    Day 22 part 2

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,4 +1,4 @@
-module Day22 where
+module Day22 (part1, part2, parse) where
 
 import Control.Monad
 import Control.Monad.ST
@@ -14,13 +14,6 @@ import qualified Relude.Unsafe as Unsafe
 import Text.ParserCombinators.ReadP
 
 type Coord = ((Int, Int), Int)
-
-data Piece = Piece
-  { piece :: Coord,
-    pieceSize :: Int,
-    pieceId :: Int
-  }
-  deriving (Show)
 
 x, y, z :: Coord -> Int
 x = fst . fst
@@ -45,14 +38,11 @@ brickToCoordList (((x1, y1), z1), ((x2, y2), z2)) =
     z2 - z1 + 1
   )
 
-type St = (Array (Int, Int) (Int, Int), M.Map Int [Int])
-
 push' :: (STArray s (Int, Int) (Int, Int), M.Map Int [Int]) -> (Int, ([(Int, Int)], Int)) -> ST s (STArray s (Int, Int) (Int, Int), M.Map Int [Int])
 push' (grid, supports) (n, (coords, h)) = do
   gridValues <- traverse (readArray grid) coords
   let height = maximum $ fst <$> gridValues
       supporters = nub . sort $ snd <$> filter ((== height) . fst) gridValues
-      -- supports' = M.unionWith (++) supports $ M.fromList [(s, [n]) | s <- supporters]
       supports' = M.insert n supporters supports
   traverse_ (\p -> writeArray grid p (height + h, n)) coords
   pure (grid, supports')
@@ -79,3 +69,21 @@ part1 p =
       (_, m) = process queue grid supports
       needed = filter (/= -1) . nub . sort . getNeeded $ M.toList m
    in length sorted - length needed
+
+part2 :: Problem -> Int
+part2 p =
+  let sorted = sortOn (z . fst) p
+      mx = maximum (x . fst <$> sorted)
+      my = maximum (y . fst <$> sorted)
+      supports = M.fromList [(l, []) | l <- [1 .. length sorted]]
+      grid = Data.Array.Base.listArray ((0, 0), (my, mx)) $ repeat (0, -1) :: Array (Int, Int) (Int, Int)
+      queue = (zip [1 ..] $ brickToCoordList <$> sorted)
+      (_, m) = process queue grid supports
+      m' = filter ((/= [-1]) . snd) $ M.toList m
+   in sum [pred . length $ calcFalls m' [n] | n <- [1 .. length sorted]]
+
+calcFalls :: [(Int, [Int])] -> [Int] -> [Int]
+calcFalls [] fallen = fallen
+calcFalls ((a, b) : xs) fallen
+  | all (`elem` fallen) b = calcFalls xs (a : fallen)
+  | otherwise = calcFalls xs fallen
