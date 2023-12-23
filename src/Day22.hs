d0882fbbf11215525f0aa65e2f6a56b74f68227b
LeixB <abone9999@gmail.com>
2023-12-23 02:34:14 +0100

    Day 22 more clean up

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,13 +1,13 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day22 (part1, part2, parse) where
 
 import Control.Monad
 import Control.Monad.ST
-import Data.Array
 import Data.Array.ST
 import Data.Char (isDigit)
-import Data.Foldable (Foldable (maximum))
+import Data.Foldable (Foldable (maximum, minimum))
 import Data.List (nub)
-import qualified Data.Map as M
 import Relude
 import qualified Relude.Unsafe as Unsafe
 import Text.ParserCombinators.ReadP
@@ -21,9 +21,7 @@ z = snd
 
 type Brick = (Coord, Coord)
 
-type Problem = [Brick]
-
-parse :: ByteString -> Maybe Problem
+parse :: ByteString -> Maybe [Brick]
 parse = fmap fst . viaNonEmpty last . readP_to_S (brick `sepBy` char '\n') . decodeUtf8
   where
     pair p sep = (,) <$> p <* char sep <*> p
@@ -31,44 +29,42 @@ parse = fmap fst . viaNonEmpty last . readP_to_S (brick `sepBy` char '\n') . dec
     coord = (,) <$> pair int ',' <* char ',' <*> int
     int = Unsafe.read <$> munch1 isDigit
 
-brickToCoordList :: Brick -> ([(Int, Int)], Int)
+-- x and y coordinates of points in the brick and height.
+type CoordListWithHeight = ([(Int, Int)], Int)
+
+-- fst, brick id; snd, ids of bricks that support it
+type Dependency = (Int, [Int])
+
+brickToCoordList :: Brick -> CoordListWithHeight
 brickToCoordList (((x1, y1), z1), ((x2, y2), z2)) = ([(x, y) | x <- [x1 .. x2], y <- [y1 .. y2]], z2 - z1 + 1)
 
-push :: (STArray s (Int, Int) (Int, Int), M.Map Int [Int]) -> (Int, ([(Int, Int)], Int)) -> ST s (STArray s (Int, Int) (Int, Int), M.Map Int [Int])
+push :: (STArray s (Int, Int) (Int, Int), [Dependency]) -> (Int, CoordListWithHeight) -> ST s (STArray s (Int, Int) (Int, Int), [Dependency])
 push (grid, supports) (n, (coords, h)) = do
   gridValues <- traverse (readArray grid) coords
   let height = maximum $ fst <$> gridValues
       supporters = nub . sort $ snd <$> filter ((== height) . fst) gridValues
-      supports' = M.insert n supporters supports
-  traverse_ (\p -> writeArray grid p (height + h, n)) coords
-  pure (grid, supports')
-
-process :: [(Int, ([(Int, Int)], Int))] -> Array (Int, Int) (Int, Int) -> M.Map Int [Int] -> M.Map Int [Int]
-process queue grid supports = runST $ do
-  grid' <- thaw grid
-  snd <$> foldM push (grid', supports) queue
+  traverse_ (flip (writeArray grid) (height + h, n)) coords
+  pure (grid, (n, supporters) : supports)
 
 getNeeded :: [(Int, [Int])] -> [Int]
 getNeeded = concatMap snd . filter ((== 1) . length . snd)
 
-findDeps :: Problem -> Map Int [Int]
-findDeps p = process queue grid M.empty
+findDeps :: [Brick] -> [Dependency]
+findDeps p = runST $ newArray ((minX, minY), (maxX, maxY)) (0, -1) >>= fmap snd . flip (foldM push) queue . (,[])
   where
     sorted = sortOn (z . fst) p
-    mx = maximum (x . fst <$> sorted)
-    my = maximum (y . fst <$> sorted)
-    grid = listArray ((0, 0), (my, mx)) $ repeat (0, -1)
+    ((minX, maxX), (minY, maxY)) = bimap (minimum &&& maximum) (minimum &&& maximum) . (fmap x &&& fmap y) . fmap fst $ sorted
     queue = zip [1 ..] $ brickToCoordList <$> sorted
 
-part1, part2 :: Problem -> Int
+part1, part2 :: [Brick] -> Int
 part1 p = length p - length needed
   where
-    needed = filter (/= -1) . nub . sort . getNeeded . M.toList $ findDeps p
+    needed = filter (/= -1) . nub . sort . getNeeded $ findDeps p
 part2 p = getSum $ mconcat [calcFalls m [n] | n <- [1 .. length p]]
   where
-    m = filter ((/= [-1]) . snd) . M.toList $ findDeps p
+    m = filter ((/= [-1]) . snd) . sort $ findDeps p
 
-calcFalls :: [(Int, [Int])] -> [Int] -> Sum Int
+calcFalls :: [Dependency] -> [Int] -> Sum Int
 calcFalls [] _ = mempty
 calcFalls ((a, b) : xs) fallen
   | all (`elem` fallen) b = 1 <> calcFalls xs (a : fallen)
